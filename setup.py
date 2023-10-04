ac1201d828190c85370d5509bd3ad242e8896972
Alicia Lu <lualicia88@gmail.com>
2023-10-04 14:49:40 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='labdata_query_scripts',
+    version='0.1',
+    packages=find_packages(),
+    author="Alicia Lu",
+    author_email="lualicia88@gmail.com",
+    install_requires=["numpy"],
+    license="MIT",
+    description="query toolkit for flavell lab's whole-brain imaging datasets"
+    url="https://github.com/flavell-lab/labdata_query_scripts"
+)
